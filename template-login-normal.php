17204f9734e9a5efd6e9f91403a667b0934a04f3
Eduardo Freitas <eduardorph@hotmail.com>
2022-07-22 18:04:15 -0300

    Dashboard

--- template-login-normal.php
+++ template-login-normal.php
@@ -20,27 +20,18 @@ if(st()->get_option('enable_popup_login','off') == 'on'){
 
 get_header();
 ?>
-<div class="container-fluid">
+<div class="container-fluid pd-container-fluid-margin">
     <div class="row">
         <div class="container">
             <h1 class="page-title"><?php printf(__("Login/Register on %s",'traveler'),get_bloginfo('name')) ?></h1>
         </div>
         <div class="container">
             <div class="row" data-gutter="60">
-                <div class="col-md-4 mt30">
-                    <h3><?php the_title(); ?></h3>
-                    <?php
-                    while(have_posts()){
-                        the_post();
-                        the_content();
-                    }
-                    ?>
-                </div>
-                <div class="col-md-4 mt30">
+                <div class="col-md-6 mt30">
                     <h3><?php esc_html_e('Login','traveler') ?></h3>
                     <?php echo st()->load_template('login/form','login') ?>
                 </div>
-                <div class="col-md-4 mt30">
+                <div class="col-md-6 mt30">
                     <h3 class="pb30 mb0"><?php printf(__("New To %s ?",'traveler'),get_bloginfo('title')) ?></h3>
                     <?php   $page_user_register = st()->get_option("page_user_register");?>
                     <div class="mt5"><b><a class="btn-lg btn btn-primary" href="<?php echo get_permalink($page_user_register) ?>"><?php _e("Register New",'traveler') ?></a></b></div>
--- template-register.php
+++ template-register.php
@@ -20,14 +20,14 @@ if(st()->get_option('enable_popup_login','off') == 'on'){
 
 get_header();
 ?>
-<div class="container-fluid">
+<div class="container-fluid pd-container-fluid-margin">
     <div class="row">
         <div class="container">
             <h1 class="page-title text-center mt60"><?php printf(__("Register on %s",'traveler'),get_bloginfo('name')) ?></h1>
         </div>
         <div class="container">
             <div class="row" data-gutter="60">
-                <div class="col-md-8 col-md-offset-2">
+                <div class="col-md-12 col-md-offset-2">
                     <?php echo st()->load_template('login/form-new','login') ?>
                 </div>
             </div>
